3663952202da8815e2813440acd231f31c51d63f
Nazmus Sakib <nazmusweb.coding@gmail.com>
2024-11-08 21:05:57 +0600

    added codes to Module 1

---
+++ Module 1/My_first_code/main.py
@@ -0,0 +1,19 @@
+x = 10
+y = 20
+
+# This is a single-line comment
+
+sum = x + y
+sub = x - y
+mul = x * y
+div = x / y
+
+"""
+This is a multi-line comment
+that spans multiple lines.
+"""
+
+print(sum)
+print(sub)
+print(mul)
+print(div)
