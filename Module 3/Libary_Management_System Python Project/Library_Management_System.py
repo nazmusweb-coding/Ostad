68c0811fecf5e775eba03a58c4e4c49c4d63e731
Nazmus Sakib <nazmusweb.coding@gmail.com>
2024-11-22 20:07:54 +0600

    Module 3 - Project done

---
+++ Module 3/Libary_Management_System Python Project/Library_Management_System.py
@@ -0,0 +1,40 @@
+import add_books
+import view_all_books
+import update_books
+import remove_books
+
+all_books =[]
+
+while True:
+    print(f"\nWelcome to our Library Management System! Please select below menu ID")
+    print("Manu ID=0. Exit.")
+    print("Manu ID=1. Add Books.")
+    print("Manu ID=2. Update your Book.")
+    print("Manu ID=3. Remove a Select Book.")
+    print("Manu ID=4. View All Books.")
+
+    menu = input(f"\nSelect any Number: ")
+
+    if menu == "1":
+       all_books = add_books.add_books(all_books)
+
+    elif menu == "2":
+        update_books = update_books.update_books(all_books)
+
+    elif menu == "3":
+        remove_books = remove_books.remove_books(all_books)
+
+    elif menu == "4":
+        view_all_books.view_all_books(all_books)
+
+    elif menu == "0":
+        print(f"\nThanks for using our Library Management System!")
+        break
+        
+    else:
+        print("Please Select a Valid Number")
+
+
+
+
+
